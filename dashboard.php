d9df57543f785b6e8352097bb836268293f96b4e
adeife16 <dadaadeife@gmail.com>
2023-01-22 23:38:32 +0100

    added chart to dashboard

--- dashboard.php
+++ dashboard.php
@@ -81,7 +81,7 @@ require_once 'header.php';
   <div class="col-md-12">
     <div class="card shadow mb-4">
       <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold color-blue">Area Chart</h6>
+          <h6 class="m-0 font-weight-bold color-blue">Sales Chart (This Year)</h6>
       </div>
       <div class="card-body">
           <div class="chart-area">
